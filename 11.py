e864ea5391f0c007d47fa107cbcee905e3c79399
Jessica Chouhan <87466412+Jessica-0103@users.noreply.github.com>
2023-04-23 22:11:37 +0530

    Create 11.py

---
+++ 11.py
@@ -0,0 +1,12 @@
+Load MNIST Handwritten Digit Dataset  
+
+ 
+ from tensorflow.keras.datasets import mnist 
+# Jessica Chouhan 0827CI201087   
+from tensorflow.keras.models 
+import Sequential from tensorflow.keras.layers 
+import Conv2D, MaxPooling2D, Flatten, Dense   
+# Define the CNN architecture  
+ 
+# Jessica Chouhan 0827CI201087   
+# Evaluate the model on the test set test_loss, test_acc = model.evaluate(x_test, tf.keras.utils.to_categorical(y_test)) print('Test accuracy:', test_acc)  
